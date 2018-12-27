96bb1b416cbecbacac588e38056a48272fca21ac
“uliangzhaoxing <z342206534@163.com>
2018-12-27 13:35:14 +0800

    配置首页

--- src/main.js
+++ src/main.js
@@ -6,6 +6,11 @@ import router from './router'
 
 Vue.config.productionTip = false
 
+// 导入全局的样式
+// 导入bootstrap的样式
+import 'bootstrap/dist/css/bootstrap.css'
+import './assets/css/index.css'
+
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
